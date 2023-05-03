cb0f15632d8e4669c463e25df65d596a38cfb5dc
Reggeditt <tkodjosarso@gmail.com>
2023-05-03 16:23:22 +0000

    setup browser router with routes

--- src/App.js
+++ src/App.js
@@ -1,8 +1,18 @@
 import './App.css';
+import { Route, Routes } from 'react-router-dom';
+import HomePage from './pages/HomePage';
+import DetailPage from './pages/DetailPage';
 
 function App() {
   return (
-    <div className="App"><h1>This is where we start</h1></div>
+    <div className="App">
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="*" element={<HomePage />} />
+        <Route path="/home" element={<HomePage />} />
+        <Route path="/details/:continent" element={<DetailPage />} />
+      </Routes>
+    </div>
   );
 }
 
