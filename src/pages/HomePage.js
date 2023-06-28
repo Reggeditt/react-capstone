a64ce3dbc393d54d63c764a89e97bb2156f3eb10
Reggeditt <tkodjosarso@gmail.com>
2023-06-28 06:26:10 +0000

    render data from new API

--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -1,84 +1,34 @@
-import { useEffect } from 'react';
-import { useDispatch, useSelector } from 'react-redux';
+import { useSelector } from 'react-redux';
 import { Link } from 'react-router-dom';
-import { fetchData } from '../redux/data/dataSlice';
+import {
+  Image,
+  NavBar,
+  Space,
+  Toast,
+} from 'antd-mobile';
+
+const back = () => Toast.show({
+  content: 'Clicked the return area',
+  duration: 1000,
+});
 
 const HomePage = () => {
-  const dispatch = useDispatch();
-  const countriesData = useSelector((store) => store.data);
-  const defaultContinents = [
-    {
-      continent: 'Africa',
-      countries: [],
-    },
-    {
-      continent: 'North America',
-      countries: [],
-    },
-    {
-      continent: 'South America',
-      countries: [],
-    },
-    {
-      continent: 'Antarctica',
-      countries: [],
-    },
-    {
-      continent: 'Asia',
-      countries: [],
-    },
-    {
-      continent: 'Europe',
-      countries: [],
-    },
-    {
-      continent: 'Oceania',
-      countries: [],
-    },
-  ];
+  const tvShows = useSelector((store) => store.data);
 
-  defaultContinents.map((item) => {
-    item.countries = countriesData.filter((country) => country.continents[0] === item.continent);
-    return item;
-  });
-
-  const continents = defaultContinents;
-
-  useEffect(() => {
-    dispatch(fetchData());
-  }, [dispatch]);
   return (
-    <>
-      <div className="continent-cards-wrap">
-        <div className="banner">
-          <h1>Continents</h1>
-          <p>Click on a continent to see details about their populations and Land mass</p>
-        </div>
-        <p className="divider">Stats by continent</p>
-        <div className="continent-cards">
-          {
-            continents.map((continent) => (
-              <Link to={`/details/${continent.continent}`} key={continent.continent} className="continent-card">
-                <h1>{continent.continent}</h1>
-                <span>
-                  population:
-                  {
-                    continent.countries.reduce((total, country) => total + country.population, 0)
-                  }
-                </span>
-                <span>
-                  total Area:
-                  {
-                    continent.countries.reduce((total, country) => total + country.area, 0)
-                  }
-                </span>
-                <Link to={`/details/${continent.continent}`}>See more</Link>
-              </Link>
-            ))
-          }
-        </div>
-      </div>
-    </>
+    <div>
+      <NavBar onBack={back}>TV Shows</NavBar>
+      <Space
+        wrap
+        style={{ width: '100%', maxWidth: '700px' }}
+      >
+        {tvShows.map((show) => (
+          <Link to={`details/${show.id}`} key={show.id}>
+            <Image key={show.id} src={show.image.medium} />
+          </Link>
+        ))}
+      </Space>
+    </div>
   );
 };
 
