63c75c503fc73538e8e562a059857cf9f5e25440
Reggeditt <tkodjosarso@gmail.com>
2023-06-28 06:27:33 +0000

    add detailsSlice to store

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,9 +1,11 @@
 import { configureStore } from '@reduxjs/toolkit';
 import dataReducer from './data/dataSlice';
+import detailsReducer from './details/detailsSlice';
 
 const store = configureStore({
   reducer: {
     data: dataReducer,
+    details: detailsReducer,
   },
 });
 
