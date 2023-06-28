96a98c4392c8fbcc9a2408dc0ecbfd3606c0a8c8
Reggeditt <tkodjosarso@gmail.com>
2023-06-28 06:32:05 +0000

    reset styles

--- src/App.js
+++ src/App.js
@@ -1,16 +1,24 @@
 import './App.css';
 import { Route, Routes } from 'react-router-dom';
+import { useDispatch } from 'react-redux';
+import { useEffect } from 'react';
 import HomePage from './pages/HomePage';
 import DetailPage from './pages/DetailPage';
+import { fetchTvShows } from './redux/data/dataSlice';
 
 function App() {
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    dispatch(fetchTvShows());
+  }, [dispatch]);
   return (
     <div className="App">
       <Routes>
         <Route path="/" element={<HomePage />} />
         <Route path="*" element={<HomePage />} />
         <Route path="/home" element={<HomePage />} />
-        <Route path="/details/:continent" element={<DetailPage />} />
+        <Route path="/details/:id" element={<DetailPage />} />
       </Routes>
     </div>
   );
