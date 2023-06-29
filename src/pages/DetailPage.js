949e15a8afbc21b65f71e618bff8e8389abdcf96
Reggeditt <tkodjosarso@gmail.com>
2023-06-29 11:42:54 +0000

    add styles

--- src/pages/DetailPage.js
+++ src/pages/DetailPage.js
@@ -1,13 +1,20 @@
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { Link, useNavigate, useParams } from 'react-router-dom';
 import {
   Card,
+  Grid,
   Image,
   NavBar,
   Space,
 } from 'antd-mobile';
+import { GridItem } from 'antd-mobile/es/components/grid/grid';
+import { BiSolidSlideshow } from 'react-icons/bi';
+import { CiSettings } from 'react-icons/ci';
+import { BsFillMicFill } from 'react-icons/bs';
+import useWindowDimensions from '../components/useWindowDimensions';
 import { fetchShowData, setShow } from '../redux/details/detailsSlice';
+import styles from '../global.module.css';
 
 const DetailPage = () => {
   const navigate = useNavigate();
@@ -15,62 +22,83 @@ const DetailPage = () => {
   const { id } = useParams();
   const currentShow = useSelector((store) => store.data)
     .filter((show) => show.id === +id);
-
-  const { showData, relatedShows } = useSelector((store) => store.details);
+  const { width } = useWindowDimensions();
+  const [cols, setCols] = useState(1);
+  const { relatedShows } = useSelector((store) => store.details);
   const back = () => (
     navigate('/about', { replace: true }, { state: { from: 'detail' } })
   );
 
+  function truncateStringByWords(string, maxWords) {
+    const words = string.split(' ');
+    if (words.length <= maxWords) {
+      return string;
+    }
+    return `${words.slice(0, maxWords).join(' ')}...`;
+  }
+
   useEffect(() => {
     dispatch(setShow(currentShow[0].name));
     dispatch(fetchShowData());
-  }, [dispatch]);
-  console.log(currentShow[0]);
+    if (width > 375) setCols(3);
+    else if (width <= 375) setCols(1);
+  }, [dispatch, width]);
   return (
-    <div>
-      <NavBar onBack={back}>{`showing details for ${currentShow[0].name}`}</NavBar>
-      {
-        currentShow ? currentShow.map((show) => (
-          <Space key={show.id}>
-            <Image src={show.image.medium} />
-            <div>
-              <span>{show.status}</span>
-              <h1>{show.name}</h1>
-              <p>{show.summary}</p>
-              <p>{show.genres.map((genre) => <span key={genre}>{genre}</span>)}</p>
-              <span>{show.rating.average}</span>
-              <span>{show.language}</span>
-              <span>{show.officialSite}</span>
-            </div>
+    <div className={styles.main}>
+      <NavBar
+        left={<BiSolidSlideshow size={25} color="white" />}
+        right={(
+          <Space>
+            <CiSettings size={22} color="white" />
+            <BsFillMicFill size={22} color="white" />
           </Space>
-        )) : showData.map((show) => (
-          <Space key={show.show.id}>
-            <Image src={show.show.image.medium} />
-            <div>
-              <span>{show.show.status}</span>
-              <h1>{show.show.name}</h1>
-              <p>{show.show.summary}</p>
-              <p>{show.show.genres.map((genre) => <span key={genre}>{genre}</span>)}</p>
-              <span>{show.show.rating.average}</span>
-              <span>{show.show.language}</span>
-              <span>{show.show.officialSite}</span>
-            </div>
+        )}
+        onBack={back}
+      >
+        {`showing details for ${currentShow[0].name}`}
+      </NavBar>
+
+      <div
+        className={styles.container}
+        style={{ width: '100%', maxWidth: '1024px', margin: '0 auto' }}
+      >
+        {
+          currentShow ? currentShow.map((show) => (
+            <Grid columns={cols} gap={8} key={show.id} style={{ width: '90%', margin: '0', maxHeight: cols > 1 ? '450px' : null }}>
+              <GridItem span={1}><Image src={show.image.medium} /></GridItem>
+              <GridItem span={cols < 3 ? 1 : 2}>
+                <div className={styles.showDetails}>
+                  <span style={{ fontSize: '0.8rem' }}>{show.status}</span>
+                  <h1>{show.name}</h1>
+                  <p
+                    style={{ textJustify: 'inter-word', textAlign: 'justify', fontSize: '1rem' }}
+                  >
+                    {truncateStringByWords(show.summary, 50).replace(/<[^>]*>/g, '')}
+                  </p>
+                  <Space style={{ fontSize: '0.8rem' }}>
+                    {show.genres.map((genre) => <span key={genre}>{genre}</span>)}
+                  </Space>
+                  <span style={{ fontSize: '0.8rem' }}>{`rating: ${show.rating.average}`}</span>
+                  <span style={{ fontSize: '0.8rem' }}>{`language: ${show.language}`}</span>
+                </div>
+              </GridItem>
+            </Grid>
+          )) : <div>Details not found</div>
+        }
+        <Card title="Related Movies" style={{ width: '100%', backgroundColor: 'black', color: 'white' }}>
+          <Space className="detail-card" style={{ overflow: 'scroll' }}>
+            {relatedShows.length ? relatedShows.map((show) => (
+              <div key={show.show.id}>
+                <Image src={show.show.image.medium} />
+              </div>
+            )) : <div>No related shows found</div>}
           </Space>
-        ))
-      }
-      <Card title="Related Movies">
-        <Space className="detail-card">
-          {relatedShows.length ? relatedShows.map((show) => (
-            <div key={show.show.id}>
-              <Image src={show.show.image.medium} />
-            </div>
-          )) : <div>No related shows found</div>}
-        </Space>
-      </Card>
-      <Link to="/">
-        {'<'}
-        back to continents
-      </Link>
+        </Card>
+        <Link to="/">
+          {'<'}
+          back to continents
+        </Link>
+      </div>
     </div>
   );
 };
--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -1,33 +1,60 @@
 import { useSelector } from 'react-redux';
 import { Link } from 'react-router-dom';
+import { useEffect, useState } from 'react';
 import {
+  Grid,
   Image,
   NavBar,
   Space,
-  Toast,
 } from 'antd-mobile';
-
-const back = () => Toast.show({
-  content: 'Clicked the return area',
-  duration: 1000,
-});
+import { GridItem } from 'antd-mobile/es/components/grid/grid';
+import { BiSolidSlideshow } from 'react-icons/bi';
+import { CiSettings } from 'react-icons/ci';
+import { BsFillMicFill } from 'react-icons/bs';
+import styles from '../global.module.css';
+import useWindowDimensions from '../components/useWindowDimensions';
 
 const HomePage = () => {
   const tvShows = useSelector((store) => store.data);
+  const { width } = useWindowDimensions();
+  const [cols, setCols] = useState(2);
+
+  useEffect(() => {
+    if (width <= 425) setCols(2);
+    else if (width > 425 && width <= 768) setCols(3);
+    else if (width > 768 && width < 1024) setCols(4);
+    else setCols(5);
+  }, [width]);
 
   return (
-    <div>
-      <NavBar onBack={back}>TV Shows</NavBar>
-      <Space
-        wrap
-        style={{ width: '100%', maxWidth: '700px' }}
+    <div className={styles.main}>
+      <NavBar
+        left={<BiSolidSlideshow size={25} color="white" />}
+        right={(
+          <Space>
+            <CiSettings size={22} color="white" />
+            <BsFillMicFill size={22} color="white" />
+          </Space>
+        )}
+        backArrow={false}
+        className={styles.nav}
       >
-        {tvShows.map((show) => (
-          <Link to={`details/${show.id}`} key={show.id}>
-            <Image key={show.id} src={show.image.medium} />
-          </Link>
-        ))}
-      </Space>
+        TV Shows
+      </NavBar>
+      <div
+        className={styles.container}
+        style={{ maxWidth: '1024px', margin: '0 auto' }}
+      >
+        <Grid columns={cols} gap={2} wrap className={styles.container}>
+          {tvShows.map((show) => (
+            <GridItem key={show.id}>
+              <Link to={`details/${show.id}`}>
+                <Image key={show.id} src={show.image.medium} />
+              </Link>
+            </GridItem>
+          ))}
+        </Grid>
+      </div>
     </div>
   );
 };
